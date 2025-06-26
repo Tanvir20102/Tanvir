bea83246dfd2e037849382eb55695371a13b18ab
Tanvir20102 <165181121+Tanvir20102@users.noreply.github.com>
2025-06-26 14:14:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+// // // animating navbar
+
+// var tl = gsap.timeline();
+
+//     // tl.from("header nav", {
+//     //     y: "-100%",
+//     //     duration: 1,
+//     //     opacity: 0,
+//     //     ease: "elastic.out(1,1)",
+//     // })
+
+//     tl.from("header nav .nav-container .company-logo img, header nav .nav-container .nav-links-container ul a", {
+//         y: 20,
+//         opacity: 0,
+//         // ease: "power4.out",
+//         delay: 0.5,
+//         stagger: 0.3
+//     })
+
+//     tl.from("header .quote-container .quote h1", {
+//         opacity: 0,
+//         scale: 0.97,
+//         y: 20
+//     })
+
+// gsap.from(".contact-section .contact-wrapper .contact-text h1", {
+//   x: -150,
+//   opacity: 0,
+//   scrollTrigger: {
+//     trigger: ".contact-section .contact-wrapper .contact-text h1",
+//     start: "top 70%",
+//     end: "top 50%",
+//     scrub: 2,
+//     markers: true
+//   }
+// });
+
+// gsap.from(".contact-section .contact-wrapper .contact-text .para p", {
+//   x: -150,
+//   opacity: 0,
+//   scrollTrigger: {
+//     trigger: ".contact-section .contact-wrapper .contact-text .para p",
+//     start: "top 70%",
+//     end: "top 50%",
+//     scrub: 2,
+//     markers: true
+//   }
+// });
+
+// gsap.from(".contact-section .contact-wrapper .form form", {
+//   x: 150,
+//   opacity: 0,
+//   scrollTrigger: {
+//     trigger: ".contact-section .contact-wrapper .form form",
+//     start: "top 50%",
+//     end: "top 30%",
+//     scrub: 2,
+//     markers: true
+//   }
+// });
+
+// window.addEventListener("load", () => {
+//   ScrollTrigger.refresh();
+// });
+
+
+
+
+    
